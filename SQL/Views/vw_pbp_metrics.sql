309beb873228d21e6dc99e43bf1e92bdc655966f
Abir Deb <adeb@zipcar.com>
2018-10-23 15:00:23 -0400

    adding cumulative successful plays for current quarter

--- SQL/Views/vw_pbp_metrics.sql
+++ SQL/Views/vw_pbp_metrics.sql
@@ -33,6 +33,7 @@ WITH primary_key AS (
          , SUM(CASE "type" WHEN 'PASS' THEN 1 ELSE 0 END) OVER (PARTITION BY gid, off, qtr ORDER BY pid) AS cum_pass_attempts_curr_qtr
          , SUM(CASE "type" WHEN 'PASS' THEN yds ELSE 0 END) OVER (PARTITION BY gid, off, qtr ORDER BY pid) AS cum_pass_yards_curr_qtr
          , SUM(CASE succ WHEN 'Y' THEN 1 ELSE 0 END) OVER (PARTITION BY gid, off ORDER BY pid) AS cum_succ_plays
+         , SUM(CASE succ WHEN 'Y' THEN 1 ELSE 0 END) OVER (PARTITION BY gid, off, qtr ORDER BY pid) AS cum_succ_plays_curr_qtr
          , SUM(CASE WHEN succ = 'Y' AND "type" = 'RUSH' THEN 1 ELSE 0 END) OVER (PARTITION BY gid, off ORDER BY pid) AS cum_succ_run_plays
          , SUM(CASE WHEN succ = 'Y' AND "type" = 'PASS' THEN 1 ELSE 0 END) OVER (PARTITION BY gid, off ORDER BY pid) AS cum_succ_pass_plays
     FROM pbp
@@ -61,6 +62,7 @@ WITH primary_key AS (
          , SUM(CASE "type" WHEN 'PASS' THEN 1 ELSE 0 END) OVER (PARTITION BY gid, off, half ORDER BY pid) AS cum_pass_attempts_curr_half
          , SUM(CASE "type" WHEN 'PASS' THEN yds ELSE 0 END) OVER (PARTITION BY gid, off, half ORDER BY pid) AS cum_pass_yards_curr_half
          , cum_succ_plays
+         , cum_succ_plays_curr_qtr
          , cum_succ_run_plays
          , cum_succ_pass_plays
     FROM rolling_stats
@@ -86,6 +88,7 @@ SELECT pid
        , cum_pass_yards_curr_half
        , ROUND(cum_pass_yards_curr_half * 1.0 / cum_pass_attempts_curr_half, 2) AS yards_per_pass_attempt_curr_half
        , cum_succ_plays
+       , cum_succ_plays_curr_qtr
        , cum_succ_run_plays
        , cum_succ_pass_plays
   FROM sub
